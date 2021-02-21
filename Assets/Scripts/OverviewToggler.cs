521780aa3357ddd05fbdd06846add3d7c145b08d
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-21 16:36:34 -0500

    Overview screen toggle input

---
+++ Assets/Scripts/OverviewToggler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+public class OverviewToggler : MonoBehaviour
+{
+    public BoolVariable OverviewScreenActive;
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Toggle Overview Screen"))
+        {
+            OverviewScreenActive.Value = !OverviewScreenActive.Value;
+        }
+    }
+}
