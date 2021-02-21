cfec784b8ab504279a58cadf4c6a436815428460
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-21 12:40:55 -0500

    Camera movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 OffsetFromTarget;
+    public float FollowDelay;
+
+    Vector3 velocity;
+
+    void Update ()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, Target.position + OffsetFromTarget, ref velocity, FollowDelay);
+    }
+}
