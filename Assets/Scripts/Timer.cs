a4d9c29f1306f0c0b83b88471af2d0877d1da94b
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-23 01:04:39 -0500

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float CurrentTime { get; private set; }
+
+    public float MaxTime;
+
+    public TextMeshProUGUI Text;
+    public BoolVariable DeathScreenActive;
+
+    void Start ()
+    {
+        OnPlayerRespawn();
+    }
+
+    void Update ()
+    {
+        if (DeathScreenActive.Value) return;
+
+        CurrentTime -= Time.deltaTime;
+
+        Text.text = TimeSpan.FromSeconds(Mathf.Round(CurrentTime)).ToString(@"m\:ss");
+
+        if (CurrentTime <= 0)
+        {
+            DeathLoopManager.Instance.EnterScreen();
+        }
+    }
+
+    public void OnPlayerRespawn ()
+    {
+        CurrentTime = MaxTime;
+    }
+}
