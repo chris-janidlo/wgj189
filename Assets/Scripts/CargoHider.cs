bceb5e9e02e6d443eda6f9acf9ed90cb046f545c
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-23 15:22:32 -0500

    Resize UI for new 1024x768 resolution, hide cargo when moused over

---
+++ Assets/Scripts/CargoHider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using crass;
+
+public class CargoHider : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public float MouseOverAlpha;
+    public TransitionableFloat AlphaTransition;
+    public CanvasGroup CanvasGroup;
+
+    void Start ()
+    {
+        AlphaTransition.AttachMonoBehaviour(this);
+    }
+
+    void Update ()
+    {
+        CanvasGroup.alpha = AlphaTransition.Value;
+    }
+
+    public void OnPointerEnter (PointerEventData eventData)
+    {
+        AlphaTransition.StartTransitionTo(MouseOverAlpha);
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+        AlphaTransition.StartTransitionTo(1);
+    }
+}
