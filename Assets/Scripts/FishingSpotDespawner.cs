dfc91e183956a4ee242775e43382c82f9be569f3
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-25 01:06:16 -0500

    Despawn fishing spot on death

---
+++ Assets/Scripts/FishingSpotDespawner.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class FishingSpotDespawner : MonoBehaviour
+{
+    void Start ()
+    {
+        DeathLoopManager.Instance.OnPlayerDied.AddListener(() => DestroyObject(gameObject));
+    }
+}
