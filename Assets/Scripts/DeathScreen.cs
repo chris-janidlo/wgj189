bf139553a43a887f8498013162b3387f03a541ca
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-26 15:32:29 -0500

    Nicen the death screen

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -24,21 +24,21 @@ public class DeathScreen : MonoBehaviour
 
         LastDaySummary.text = "";
 
-        if (haul.Firefishes > 0) LastDaySummary.text += $"Firefishes: {haul.Firefishes}\n";
-        if (haul.Icefishes > 0) LastDaySummary.text += $"Icefishes: {haul.Icefishes}\n";
-        if (haul.Poisonfishes > 0) LastDaySummary.text += $"Poisonfishes: {haul.Poisonfishes}\n";
-        if (haul.Spikefishes > 0) LastDaySummary.text += $"Spikefishes: {haul.Spikefishes}\n";
-        if (haul.Bonefishes > 0) LastDaySummary.text += $"Bonefishes: {haul.Bonefishes}\n";
+        if (haul.Firefishes > 0) LastDaySummary.text += $"Firefishes - {haul.Firefishes}\n";
+        if (haul.Icefishes > 0) LastDaySummary.text += $"Icefishes - {haul.Icefishes}\n";
+        if (haul.Poisonfishes > 0) LastDaySummary.text += $"Poisonfishes - {haul.Poisonfishes}\n";
+        if (haul.Spikefishes > 0) LastDaySummary.text += $"Spikefishes - {haul.Spikefishes}\n";
+        if (haul.Bonefishes > 0) LastDaySummary.text += $"Bonefishes - {haul.Bonefishes}\n";
 
         var goal = OverallGoal.Instance.Goal;
         var progress = OverallGoal.Instance.Progress;
 
         OverallSummary.text =
-$@"Firefishes: {progress.Firefishes}/{goal.Firefishes}
-Icefishes: {progress.Icefishes}/{goal.Icefishes}
-Poisonfishes: {progress.Poisonfishes}/{goal.Poisonfishes}
-Spikefishes: {progress.Spikefishes}/{goal.Spikefishes}
-Bonefishes: {progress.Bonefishes}/{goal.Bonefishes}";
+$@"Firefishes - {progress.Firefishes}/{goal.Firefishes}
+Icefishes - {progress.Icefishes}/{goal.Icefishes}
+Poisonfishes - {progress.Poisonfishes}/{goal.Poisonfishes}
+Spikefishes - {progress.Spikefishes}/{goal.Spikefishes}
+Bonefishes - {progress.Bonefishes}/{goal.Bonefishes}";
 
         StartCoroutine(screenRoutine());
     }
