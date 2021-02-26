7d9e51b64e5e9189766208f2894fd0913fd718f4
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-26 00:11:49 -0500

    Health bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using crass;
+
+public class HealthBar : MonoBehaviour
+{
+    public TransitionableFloat JuiceAnimation;
+    public Image Bar;
+
+    void Start ()
+    {
+        JuiceAnimation.AttachMonoBehaviour(this);
+    }
+
+    void Update ()
+    {
+        float percent = Player.Instance.CurrentHealth / Player.Instance.MaxHealth;
+
+        if (Bar.fillAmount != percent && !JuiceAnimation.Transitioning)
+        {
+            JuiceAnimation.StartTransitionTo(percent);
+        }
+
+        Bar.fillAmount = JuiceAnimation.Value;
+    }
+}
