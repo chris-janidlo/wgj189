0adee54ff9d12cf889d2952091a48f0da25a3033
crass_sandwich <christopherjanidlo@gmail.com>
2021-02-28 15:19:15 -0500

    Show goldenfish haul/progress

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -31,6 +31,7 @@ public class DeathScreen : MonoBehaviour
         if (haul.Poisonfishes > 0) LastDaySummary.text += $"Poisonfishes - {haul.Poisonfishes}\n";
         if (haul.Spikefishes > 0) LastDaySummary.text += $"Spikefishes - {haul.Spikefishes}\n";
         if (haul.Bonefishes > 0) LastDaySummary.text += $"Bonefishes - {haul.Bonefishes}\n";
+        if (haul.Goldenfishes > 0) LastDaySummary.text += $"Goldenfishes - {haul.Goldenfishes}\n";
 
         var goal = OverallGoal.Instance.Goal;
         var progress = OverallGoal.Instance.Progress;
@@ -40,7 +41,8 @@ $@"Firefishes - {progress.Firefishes}/{goal.Firefishes}
 Icefishes - {progress.Icefishes}/{goal.Icefishes}
 Poisonfishes - {progress.Poisonfishes}/{goal.Poisonfishes}
 Spikefishes - {progress.Spikefishes}/{goal.Spikefishes}
-Bonefishes - {progress.Bonefishes}/{goal.Bonefishes}";
+Bonefishes - {progress.Bonefishes}/{goal.Bonefishes}
+Goldenfishes - {progress.Goldenfishes}/{goal.Goldenfishes}";
 
         StartCoroutine(screenRoutine());
     }
